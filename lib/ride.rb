f6b1fde5e3ce00c20ed691a282d9e8d22ec6d0b8
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2024-01-16 09:44:58 -0700

    Feat:ride created

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,22 @@
 class Ride
+    attr_reader :name, :distance, :terrain
 
+    def initialize(ride_attributes)
+        @name = ride_attributes[:name]
+        @distance = ride_attributes[:distance]
+        @loop = ride_attributes[:loop]
+        @terrain = ride_attributes[:terrain]
+    end
+
+    def total_distance
+        if @loop == false
+            @distance * 2
+        else
+            @distance
+        end
+    end
+
+    def loop?
+        @loop
+    end
 end
