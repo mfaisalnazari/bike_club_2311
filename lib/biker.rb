fba6a89e3b4ab99dcf8c72d692aa1f6b6ede2072
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2024-01-16 10:03:53 -0700

    biker complete

--- lib/biker.rb
+++ lib/biker.rb
@@ -11,4 +11,19 @@ class Biker
     def learn_terrain!(terrain)
         @acceptable_terrain << terrain
     end
+
+    def log_ride(ride , time)
+        if @acceptable_terrain.include?(ride.terrain) && @max_distance > ride.total_distance
+            @rides[ride] ||= []
+            @rides[ride] << time
+        end
+    end
+
+    def personal_record(ride)
+        if @rides.keys.include?(ride)
+            @rides[ride].min
+        else
+            false
+        end
+    end
 end
