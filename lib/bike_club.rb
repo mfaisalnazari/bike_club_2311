aa9741069c6a0bc9fb031c3843cffe65c9c01a97
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2024-01-16 10:42:07 -0700

    feat:methods created

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -4,4 +4,24 @@ class BikeClub
         @name = name
         @bikers = []
     end
+
+    def add_bikers(biker)
+        @bikers << biker
+    end
+
+    def eligible_bikers(ride)
+        @bikers.find_all {|biker| biker.acceptable_terrain.include?(ride.terrain) && biker.max_distance > ride.total_distance }
+    end
+
+    def most_rides_biker
+        @bikers.find do
+            |biker| biker.rides.keys.max
+        end
+    end
+
+    def best_record_biker(ride)
+        @bikers.min_by do
+            |biker| biker.personal_record(ride)
+        end
+    end
 end
